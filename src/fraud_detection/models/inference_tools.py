1d929aad7a133aa6caec3173c6646596082a9944
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-11-21 10:15:27 +0300

    Update inference_tools.py

--- src/fraud_detection/models/inference_tools.py
+++ src/fraud_detection/models/inference_tools.py
@@ -2,22 +2,34 @@ import pyspark.sql.functions as F
 from pyspark.sql.types import IntegerType, DoubleType
 
 
-def get_k_suspicious_cards(predictions, k=50):
-
+def adjust_predictions(predictions):
     def extract_prob(v):
         try:
             return float(v[1])  
         except ValueError:
             return None
-
     extract_prob_udf = F.udf(extract_prob, DoubleType())
     predictions = predictions.withColumn(
-          'prob_flag', extract_prob_udf(F.col('probability'))
-    )    
+        'prob', extract_prob_udf(F.col('probability'))
+    )
+    return predictions 
+
+
+def get_fraud_probability(predictions):
+    predictions = adjust_predictions(predictions)
+    TransactionID = predictions.select('TransactionID').collect()[0][0]
+    card1 = predictions.select('card1').collect()[0][0]
+    probability = predictions.select('prob').collect()[0][0]
+
+    return probability
+
+
+def get_k_suspicious_cards(predictions, k=50):
+    predictions = adjust_predictions(predictions)   
     suspicious_cards = (
-        predictions.select('card1', 'prob_flag')
-        .groupBy('card1').max('prob_flag')
-        .orderBy(F.col('max(prob_flag)').desc())    
+        predictions.select('card1', 'prob')
+        .groupBy('card1').max('prob')
+        .orderBy(F.col('max(prob)').desc())    
         .limit(k)        
         .toPandas()['card1']
         .tolist()    
